36d37daebdc511cc22e646a229db56f96a8a2ce0
Reece Devine <reece.devine97@gmail.com>
2021-03-29 12:12:44 -0230

    switching to routes

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ app.use(express.static("public"));
 const port = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "views/index.html")); //localhost:3000/index.html
+	res.sendFile(path.join(__dirname, "views/index.hbs")); //localhost:3000/index.html
 });
-app.get("/home", (req, res) => {
-	res.sendFile(path.join(__dirname, "views/home.html")); 
+app.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, "views/login.hbs")); 
+});
+app.get("/settings", (req, res) => {
+	res.sendFile(path.join(__dirname, "views/settings.hbs")); 
+});
+app.get("/kids", (req, res) => {
+	res.sendFile(path.join(__dirname, "views/kids.hbs")); 
 });
 
 app.listen(port, () => {
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const { createProxyMiddleware} = require('http-proxy-middleware');
+
+router.use('/api', createProxyMiddleware({
+
+    target: 'http://localhost:5000',
+    headers: {
+        accept: 'application/json, application/x-www-form-urlencoded'
+    },
+    changeOrigin: true
+}))
+
+router.get("/", (req, res)=> {
+    res.render('index', {message: "This is the index page"})
+    
+})
+
+//other routes you might use
+
+router.use((req, res) =>{
+    res.status(404);
+    res.render("error", {layout: "errorLayout.hbs", errormessage: `You lost your way fool "${req.url}" does not exsist`});  
+})
+
+module.exports = router;
